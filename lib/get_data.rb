3c18216e281c5a4e48772ef528c9494f85e8593d
Faraaz Khan <faraaz@rationalizeit.us>
2013-09-05 01:15:15 -0400

    reducing api calls to work around rate limiting

--- lib/get_data.rb
+++ lib/get_data.rb
@@ -1,11 +1,16 @@
 class GetData
+  @@failed_tasks = 0
+  @@failed_stories = 0
   def self.perform
+    @@failed_tasks = 0
+    @@failed_stories = 0
     truncate_data
     get_users
     get_projects
     get_tasks
-    get_stories
+    #get_stories
     create_sections
+    puts "Failed Tasks = #{@@failed_tasks} and Failed Stories = #{@@failed_stories}"
   end
 
   def self.truncate_data
@@ -31,21 +36,46 @@ class GetData
   def self.get_tasks
     Project.all.each do |project|
       asana_project = Asana::Project.find(project.asana_id)
-      asana_project.tasks.each do |task|
-        t = Asana::Task.find(task.id)
-        Task.create(:asana_id => t.id, :project_id => project.id, :name => t.name, :notes => t.notes,  :due_on => t.due_on) 
+      asana_project.tasks.in_groups_of(100) do |group|
+        group.each do |task|
+          create_task_from(task, project)
+        end
       end
     end
   end
 
-  def self.get_stories
-    Task.all.each do |t|
-      asana_task = Asana::Task.find(t.asana_id)
-      asana_task.stories.each do |s|
-        Story.create(:asana_id => s.id, :created_at => s.created_at, :story_type => s.type, :text => s.text, :task_id => t.id, :created_by_id => Assignee.find_by_asana_id(s.created_by.id).id)
+  def self.create_task_from(task, project)
+    begin
+      t = Task.create(:asana_id => task.id, :project_id => project.id, :name => task.name)
+      task.stories.each do |story|
+        t.stories.create(:asana_id => story.id, :created_at => story.created_at, :story_type => story.type, :text => story.text, :task_id => t.id, :created_by_id => Assignee.find_by_asana_id(story.created_by.id).id) 
       end
+    rescue 
+      @@failed_tasks += 1
+      nil
     end
   end
+
+  #def self.get_stories
+    #Task.all.each do |t|
+      #asana_task = Asana::Task.find(t.asana_id)
+      #asana_task.stories.in_groups_of(45) do |group|
+        #group.each do |s|
+         #create_story_from(s,t)
+        #end
+        #sleep 30
+      #end
+    #end
+  #end
+
+  #def self.create_story_from(s, t)
+    #begin
+      #Story.create(:asana_id => s.id, :created_at => s.created_at, :story_type => s.type, :text => s.text, :task_id => t.id, :created_by_id => Assignee.find_by_asana_id(s.created_by.id).id)
+    #rescue
+      #@@failed_stories +=1
+      #nil
+    #end
+  #end
 
   def self.create_sections
     Project.sections.each do |s|
